aef5c506df909c6d89e93f0725b9cff235a59809
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-06-19 17:07:27 -0400

    Added functions interface and class

---
+++ Source/Libraries/Adapters/openHistorian.Adapters/GrafanaFunctions/Functions.cs
@@ -0,0 +1,54 @@
+﻿using GSF.TimeSeries;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openHistorian.Adapters.GrafanaFunctions
+{
+    internal class Add : IFunctions
+    {
+        public string Regex { get; } = "Add";
+        public string Name { get; } = "Add";
+        public string Description { get; } = "Adds a decimal number to DataSourceValue";
+        public List<IParameter> Parameters { get; }
+
+        public Add()
+        {
+            // Initialize the list of parameters
+            Parameters = new List<IParameter>
+            {
+                new Parameter<decimal>
+                {
+                    Default = typeof(decimal),
+                    Description = "Decimal number to add",
+                    Required = true
+                },
+                new Parameter<DataSourceValue>
+                {
+                    Default = typeof(DataSourceValue),
+                    Description = "Series",
+                    Required = true
+                }
+            };
+        }
+
+        public void Compute(List<object> values)
+        {
+            foreach (object value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+
+    internal class Parameter<T> : IParameter
+    {
+        public Type Default { get; set; }
+        public string Description { get; set; }
+        public bool Required { get; set; }
+        public Type Type { get; } = typeof(T);
+    }
+}
